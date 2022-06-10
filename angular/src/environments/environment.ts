47d84f5e9d479b49024d9a4926e2e3feb0202975
{Yancy Garret} <{yancygarret12@gmail.com}>
2022-06-10 23:41:17 +0800

    first commit

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://stunning-beignet-b07c62.netlify.app';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://khalidanta-001-site1.dtempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false,
+  },
+  apis: {
+    default: {
+      url: 'http://khalidanta-001-site1.dtempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
